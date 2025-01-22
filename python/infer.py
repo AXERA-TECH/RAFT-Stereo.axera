2f0671019e82f8bd0fb7bb7268d153926075c94b
lihongjie <lihongjie@axera-tech.com>
2025-01-22 21:45:02 +0800

    update

--- python/infer.py
+++ python/infer.py
@@ -30,17 +30,17 @@ def parse_args() -> argparse.Namespace:
 
 def infer(left: str, right: str, model: str):
     session = InferenceSession(path_or_bytes=model, providers= ['AxEngineExecutionProvider', 'AXCLRTExecutionProvider'])
-    input_info = []
-    input_tensors = session.get_inputs()
-    for tensor in input_tensors:
-        info = dict(name=tensor.name , type=tensor.dtype, shape=tensor.shape)
-        input_info.append(info)
+    # input_info = []
+    # input_tensors = session.get_inputs()
+    # for tensor in input_tensors:
+    #     info = dict(name=tensor.name , type=tensor.dtype, shape=tensor.shape)
+    #     input_info.append(info)
 
-    H,W = input_info[0]['shape'][1:3]
-    print("input_info",input_info)
+    # H,W = input_info[0]['shape'][1:3]
+    # print("input_info",input_info)
     # del session                         # bug in axengine
-    # W=640
-    # H=256
+    W=640
+    H=256
     print("H,W",H,W)
     
     left_raw = cv2.imread(left)
@@ -57,15 +57,15 @@ def infer(left: str, right: str, model: str):
 
     assert orig_h_left == orig_h_right and orig_w_left == orig_w_right
 
-    session = InferenceSession(path_or_bytes=model, providers= ['AxEngineExecutionProvider', 'AXCLRTExecutionProvider'])
+    # session = InferenceSession(path_or_bytes=model, providers= ['AxEngineExecutionProvider', 'AXCLRTExecutionProvider'])
     
     # for i in range(1):
-    flow_up = session.run(output_names=["output"], input_feed={input_info[0]['name']:image_left, input_info[1]['name']:image_right})[0]
+    # flow_up = session.run(output_names=["output"], input_feed={input_info[0]['name']:image_left, input_info[1]['name']:image_right})[0]
 
-    # flow_up = session.run(output_names=["output"], input_feed={"x1":image_left, "x2":image_right})[0]
+    flow_up = session.run(output_names=["output"], input_feed={"x1":image_left, "x2":image_right})[0]
 
     # cannot call session twice
-    flow_up = session.run(output_names=["output"], input_feed={input_info[0]['name']:image_left, input_info[1]['name']:image_right})[0]
+    # flow_up = session.run(output_names=["output"], input_feed={"x1":image_left, "x2":image_right})[0]
 
     flow_up = cv2.resize(flow_up[0,0], (orig_w_left, orig_h_left))
     flow_up *= orig_w_left/W
